5a7f27a246ae0ee6f19a2b89a6b55b9ee6593272
Alok Menghrajani <441307+alokmenghrajani@users.noreply.github.com>
2022-06-21 22:37:48 +0200

    set 5, challenge 34

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/alokmenghrajani/go-cryptopals/set1"
 	"github.com/alokmenghrajani/go-cryptopals/set2"
 	"github.com/alokmenghrajani/go-cryptopals/set3"
@@ -9,46 +11,89 @@ import (
 )
 
 func main() {
+	challenge := ""
+	if len(os.Args) >= 2 {
+		challenge = os.Args[1]
+	}
+	switch challenge {
 	// Set 1: Basics
-	set1.Challenge1()
-	set1.Challenge2()
-	set1.Challenge3()
-	set1.Challenge4()
-	set1.Challenge5()
-	set1.Challenge6()
-	set1.Challenge7()
-	set1.Challenge8()
+	case "1":
+		set1.Challenge1()
+	case "2":
+		set1.Challenge2()
+	case "3":
+		set1.Challenge3()
+	case "4":
+		set1.Challenge4()
+	case "5":
+		set1.Challenge5()
+	case "6":
+		set1.Challenge6()
+	case "7":
+		set1.Challenge7()
+	case "8":
+		set1.Challenge8()
 
 	// Set 2: Block crypto
-	set2.Challenge9()
-	set2.Challenge10()
-	set2.Challenge11()
-	set2.Challenge12()
-	set2.Challenge13()
-	set2.Challenge14()
-	set2.Challenge15()
-	set2.Challenge16()
+	case "9":
+		set2.Challenge9()
+	case "10":
+		set2.Challenge10()
+	case "11":
+		set2.Challenge11()
+	case "12":
+		set2.Challenge12()
+	case "13":
+		set2.Challenge13()
+	case "14":
+		set2.Challenge14()
+	case "15":
+		set2.Challenge15()
+	case "16":
+		set2.Challenge16()
 
 	// Set 3: Block & stream crypto
-	set3.Challenge17()
-	set3.Challenge18()
-	set3.Challenge19()
-	set3.Challenge20()
-	set3.Challenge21()
-	set3.Challenge22()
-	set3.Challenge23()
-	set3.Challenge24()
+	case "17":
+		set3.Challenge17()
+	case "18":
+		set3.Challenge18()
+	case "19":
+		set3.Challenge19()
+	case "20":
+		set3.Challenge20()
+	case "21":
+		set3.Challenge21()
+	case "22":
+		set3.Challenge22()
+	case "23":
+		set3.Challenge23()
+	case "24":
+		set3.Challenge24()
 
 	// Set 4: Stream crypto and randomness
-	set4.Challenge25()
-	set4.Challenge26()
-	set4.Challenge27()
-	set4.Challenge28()
-	set4.Challenge29()
-	set4.Challenge30()
-	set4.Challenge31()
-	set4.Challenge32()
+	case "25":
+		set4.Challenge25()
+	case "26":
+		set4.Challenge26()
+	case "27":
+		set4.Challenge27()
+	case "28":
+		set4.Challenge28()
+	case "29":
+		set4.Challenge29()
+	case "30":
+		set4.Challenge30()
+	case "31":
+		set4.Challenge31()
+	case "32":
+		set4.Challenge32()
 
 	// Set 5: Diffie-Hellman and friends
-	set5.Challenge33()
+	case "33":
+		set5.Challenge33()
+	case "34":
+		set5.Challenge34()
+	default:
+		set5.Challenge34()
+	}
 }
